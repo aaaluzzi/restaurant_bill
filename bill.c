7d87cf0add262135050a403bf51ff12882c90a21
aaaluzzi <aaaluzzi@cpp.edu>
2022-10-20 21:19:45 -0700

    Initial outline

---
+++ bill.c
@@ -0,0 +1,13 @@
+//structure for Item with name and price
+
+//table for items defined in the assignment
+
+/*main method(use parameters for command line arguments)
+    //item = random item from table
+    //print item name and price
+    //store tax
+    //print tax
+    //store tip
+    //print tip
+    //print total
+*/
